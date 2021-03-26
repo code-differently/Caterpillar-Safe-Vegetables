0614dbeb70454e1466d27d91cd05248c348de741
Katharyn Lynn Oggenfuss <koggenfuss@gmail.com>
2021-03-26 07:50:46 -0400

    All tests pass

--- PartThree.js
+++ PartThree.js
@@ -2,19 +2,68 @@
 class PartThree {
 
     findYoungest(data){
-        return data;
+        let rows = data.split("\n");
+        for (let i = 0; i < rows.length; i++) {
+            rows[i] = rows[i].split(", ");
+        }
+        let youngest = 999999;
+        let person = "";
+        for (let i = 1; i < rows.length; i++) {
+            let age = Number(rows[i][1]);
+            if(youngest > age) {
+                youngest = age;
+                person = rows[i][0];
+            }
+        }
+        return person.trim();
     }
 
     findLongestName(data){
-        return data;
+        let rows = data.split("\n");
+        for(let i = 0; i < rows.length; i++) {
+            rows[i] = rows[i].split(", ");
+        }
+        
+        let person = rows[1][0].trim();
+        let longestName = "";
+        for (let i = 1; i < rows.length; i++) {
+            person = rows[i][0].trim();
+            if ( longestName.length < person.length) {
+                longestName = person.trim();
+                console.log("LongestName = " + longestName);
+            }
+        }
+        return longestName.trim();
     }
 
     findCoOwner(data){
-        return data;
+        let rows = data.split("\n");
+        for (let i = 0; i < rows.length; i++) {
+            rows[i] = rows[i].split(", ");
+        }
+
+        let coFounder = "";
+        for (let i = 1; i < rows.length; i++) {
+            if (rows[i][2].trim() === "Co-Founder") {
+                coFounder = rows[i][0].trim();
+            }
+        }
+        return coFounder;
     }
 
     findStudents(data){
-        return data;
+        let rows = data.split("\n");
+        for (let i = 0; i < rows.length; i++) {
+            rows[i] = rows[i].split(", ");
+        }
+
+        let Student = [];
+        for (let i = 1; i < rows.length; i++) {
+            if (rows[i][2].trim() === "Student") {
+                 Student.push(rows[i][0].trim());
+            }
+        }
+        return Student;
     }
 
 }
--- PartTwo.js
+++ PartTwo.js
@@ -1,8 +1,8 @@
 
 // Part 2-0
 
-function zero(){
-
+function convertCSVToTable(){
+    // Creating a csv data file
     let data = 
         `name, age, role
         Kaleb Burd, 24, Instructor
@@ -10,21 +10,19 @@ function zero(){
         Harry Potter, 18, Student
         Gollum, 589, Student
         Bill Gates, 65, Student`;
-        
+    // Split data into rows
     let rows = data.split("\n");
-
+    // Iterating over all the rows
     for(let i = 0; i < rows.length; i++){
         rows[i] = rows[i].split(", ")
     }
-
     console.log(rows);
-
 }
 
 
 // Part 2-1
-function one(){
-    
+function sumOfAges(){
+    // Creating a csv file
     let data = 
         `name, age, role
         Kaleb Burd, 24, Instructor
@@ -32,26 +30,40 @@ function one(){
         Harry Potter, 18, Student
         Gollum, 589, Student
         Bill Gates, 65, Student`;
-        
+    // split data into rows
     let rows = data.split("\n");
-
+    // iterating over all the rows
     for(let i = 0; i < rows.length; i++){
         rows[i] = rows[i].split(", ")
     }
-
+    // Set total variable to 0
     let total = 0;
-
-    for(let i = 0; i < rows.length; i++){
+    // Iterating over all the rows
+    for(let i = 1; i < rows.length; i++){
+        // Converting a string into a number
         let age = Number(rows[i][1]);
+        // Adding age to total
         total += age;
     }
-
+        /* Iteration i    total age           age
+            1               0                  0
+            1               24                 24
+            2               24                 39
+            2               63                 39
+            3               63                 18 
+            3               81                 18
+            4               81                 589
+            4               670                589
+            5               670                 65
+            5               735                 65
+    */
+   // Print to console 735
     console.log(total);
 }
 
 // Part 2-2
-function two(){
-
+function averageAge(){
+    // Creating a csv file
     let data = 
         `name, age, role
         Kaleb Burd, 24, Instructor
@@ -59,29 +71,43 @@ function two(){
         Harry Potter, 18, Student
         Gollum, 589, Student
         Bill Gates, 65, Student`;
-        
+    // Split data into rows
     let rows = data.split("\n");
-
+    // Iterating over all the rows
     for(let i = 0; i < rows.length; i++){
         rows[i] = rows[i].split(", ")
     }
-
+    // Setting variable total to 0
     let total = 0;
-
-    for(let i = 0; i < rows.length; i++){
+    // Iterating over rows
+    for(let i = 1; i < rows.length; i++){
+        // Converting a string to a number
         let age = Number(rows[i][1]);
+        // Adding age to total
         total += age;
     }
-
-    let average = total/rows.length
-
+        /* Iteration i    total age           age
+            1               0                  0
+            1               24                 24
+            2               24                 39
+            2               63                 39
+            3               63                 18 
+            3               81                 18
+            4               81                 589
+            4               670                589
+            5               670                 65
+            5               735                 65
+    */
+    // Getting the average age by dividing total(age) by number of individual ages
+    let average = total/(rows.length-1);
+    //Print 147 to console
     console.log(average);
 
 }
 
 // Part 2-3
 function three(){
-
+    // Creating a csv file
     let data = 
         `name, age, role
         Kaleb Burd, 24, Instructor
@@ -89,29 +115,43 @@ function three(){
         Harry Potter, 18, Student
         Gollum, 589, Student
         Bill Gates, 65, Student`;
-    
+     // Split data into rows
     let rows = data.split("\n");
-
+    // Iterating over rows
     for(let i = 0; i < rows.length; i++){
         rows[i] = rows[i].split(", ")
     }
-
+    // Set variable largest to -1
     let largest = -1;
-
-    for(let i = 0; i < rows.length; i++){
+    // Iterating over rows
+    for(let i = 1; i < rows.length; i++){
+        // Convert string to a number and set to age
         let age = Number(rows[i][1]);
+        // if this is the largest age, set to largest
         if(largest < age){
             largest = age;
         }
     }
-
+        /* Iteration i    age(rows[i][1])       largest(age)
+            1               24                  -1
+            1               24                  24
+            2               39                  24
+            2               39                  39
+            3               18                  39
+            3               18                  39
+            4               589                 39
+            4               589                 589
+            5               65                  589
+            5               65                  589 
+    */
+   //Print 589 to console
     console.log(largest);
 
 }
 
 //Part 2-4
-function four(){
-
+function findLargestAge (){
+    // Creating a csv file
     let data = 
         `name, age, role
         Kaleb Burd, 24, Instructor
@@ -119,24 +159,40 @@ function four(){
         Harry Potter, 18, Student
         Gollum, 589, Student
         Bill Gates, 65, Student`;
-    
+    // Split data into rows
     let rows = data.split("\n");
-
+    // Iterating over rows
     for(let i = 0; i < rows.length; i++){
         rows[i] = rows[i].split(", ")
     }
-
+    // Setting variable largest to -1 and declaring variable person
     let largest = -1;
     let person;
-
-    for(let i = 0; i < rows.length; i++){
+    // Iterating over rows
+    for(let i =1; i < rows.length; i++){
+        //Convert string to number
         let age = Number(rows[i][1]);
+        // If largest is less than age
         if(largest < age){
+            // Declare largest to be age
             largest = age;
+            // Declare person to be person with that age
             person = rows[i][0]
         }
-    }
+    } 
+    /* Iteration i          age                 largest(age)                person
+            1               -1                   -1                         undefined
+            1               24                   24                         Kaleb Burd
+            2               24                   24                         Kaleb Burd
+            2               39                   39                         Tariq Hook
+            3               39                   39                         Tariq Hook
+            3               18                   39                         Tariq Hook
+            4               589                  39                         Tariq Hook
+            4               589                  589                        Gollum
+            5               65                   589                        Gollum
 
+    */       
+    // Print Gollum to console
     console.log(person);
 
 }
